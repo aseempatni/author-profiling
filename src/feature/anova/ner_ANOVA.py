48fd56734bffe55410c2dda06ebf0d2475be7a10
Aseem Patni <apatnip@gmail.com>
2015-10-03 14:05:35 +0530

    added anova test

---
+++ src/feature/anova/ner_ANOVA.py
@@ -0,0 +1,43 @@
+import csv
+import pandas as pd
+
+
+#dictionary with author id as key and pos distribution as value
+ner_gender = {}
+ner_age = {}
+
+
+column = {}
+columnList = []
+column['MALE']=[]
+column['FEMALE']=[]
+column['18-24']=[]
+column['25-34']=[]
+column['35-49']=[]
+column['50-64']=[]
+column['65-xx']=[]
+
+ifile = open('NER_distribution.csv')
+reader = csv.reader(ifile)
+i=0
+for row in reader:
+	if i==0:
+		i=1
+	else:
+		value = (float(row[3])*100)/max(float(row[4]),1)
+		column[row[1]].append(value)
+		column[row[2]].append(value)
+
+ifile.close()
+
+
+
+
+
+no_of_rows = max([len(column['MALE']),len(column['FEMALE']),len(column['18-24']),len(column['25-34']),len(column['35-49']),len(column['50-64']),len(column['65-xx'])])
+#creating a dataframe with author id as index and tags as columns
+df = pd.DataFrame(data=0,index=[i+1 for i in range(no_of_rows)], columns=['MALE','FEMALE','18-24','25-34','35-49','50-64','65-xx'])
+for key,value in column.items():
+	df[key] = value+([0]*(no_of_rows-len(value)))
+
+df.to_csv('ner_ANOVA.csv')
---
+++ src/feature/anova/punctuation_ANOVA.py
@@ -0,0 +1,54 @@
+import csv
+import pandas as pd
+
+
+
+column = {}
+columnList = []
+
+pList = []
+no_of_row=0
+
+
+ifile = open('punctuation_distribution.csv')
+reader = csv.reader(ifile)
+i=0
+for row in reader:
+	if i==0:
+		pList = row[3:len(row)-1]
+		for j in range(3,len(row)-1):
+			columnList.append('MALE_'+row[j])
+			columnList.append('FEMALE_'+row[j])
+			columnList.append('18-24_'+row[j])
+			columnList.append('25-34_'+row[j])
+			columnList.append('35-49_'+row[j])
+			columnList.append('50-64_'+row[j])
+			columnList.append('65-xx_'+row[j])
+			column['MALE_'+row[j]]=[]
+			column['FEMALE_'+row[j]]=[]
+			column['18-24_'+row[j]]=[]
+			column['25-34_'+row[j]]=[]
+			column['35-49_'+row[j]]=[]
+			column['50-64_'+row[j]]=[]
+			column['65-xx_'+row[j]]=[]
+		i=1
+	else:
+		for j in range(3,len(row)-1):
+			value = (float(row[j])*100)/max(float(row[len(row)-1]),1)
+			column[row[1]+'_'+pList[j-3]].append(value)
+			column[row[2]+'_'+pList[j-3]].append(value)
+			no_of_row = max([no_of_row,len(column[row[1]+'_'+pList[j-3]]),len(column[row[2]+'_'+pList[j-3]])])
+
+ifile.close()
+
+
+
+
+
+#creating a dataframe with author id as index and tags as columns
+df = pd.DataFrame(data=0,index=[i for i in range(no_of_row)], columns=columnList)
+
+for key,value in column.items():
+	df[key] = value+([0]*(no_of_row-len(value)))
+
+df.to_csv('punctuation_ANOVA.csv')
