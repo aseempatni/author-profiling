6ce4f7793e79cd2245b9d61f70a2e231ea61b624
Aseem Patni <apatnip@gmail.com>
2015-10-26 21:56:00 +0530

    added readibility to main

--- src/feature/main.py
+++ src/feature/main.py
@@ -10,11 +10,45 @@ import NER_tagger
 import pos_tagging
 import html_links
 
+from readability import readability as r
+
 directory = '../../data/pan14-author-profiling-training-corpus-2014-04-16/mnt/nfs/tira/data/pan14-training-corpora-truth/pan14-author-profiling-training-corpus-english-blogs-2014-04-16'
 outdir = 'output/'
 
 author = get_author_info(directory+'/truth.txt')
 authorFileNames = os.listdir(directory)
+
+def main(author):
+
+    for file in filter_xml(authorFileNames):
+        author_id = file.split('.')[0]
+        file_path = directory+"/"+file
+
+        # Get features
+        author[author_id]['punctuation'] = punctu.extract_from_xml(file_path)
+        author[author_id]['hyperlinks_info'] = html_links.get_hyperlink_info(file_path)
+        author[author_id]['pos'] = pos_tagging.distribution(pos_tagging.get_POS_Tags(file_path))
+        author[author_id]['NER'] = NER_tagger.get_NER_tags(file_path)
+
+        text = """We are close to wrapping up our 10 week Rails Course. This week we will cover a handful of topics commonly encountered in Rails projects. We then wrap up with part 2 of our Reddit on Rails exercise!  By now you should be hard at work on your personal projects. The students in the course just presented in front of the class with some live demos and a brief intro to to the problems their app were solving. Maybe set aside some time this week to show someone your progress, block off 5 minutes and describe what goal you are working towards, the current state of the project (is it almost done, just getting started, needs UI, etc.), and then show them a quick demo of the app. Explain what type of feedback you are looking for (conceptual, design, usability, etc.) and see what they have to say.  As we are wrapping up the course you need to be focused on learning as much as you can, but also making sure you have the tools to succeed after the class is over."""
+        # Readability scores
+        rd = r.Readability(text)
+        print 'Test text:'
+        print '"%s"\n' % text
+        print 'ARI: ', rd.ARI()
+        print 'FleschReadingEase: ', rd.FleschReadingEase()
+        print 'FleschKincaidGradeLevel: ', rd.FleschKincaidGradeLevel()
+        print 'GunningFogIndex: ', rd.GunningFogIndex()
+        print 'SMOGIndex: ', rd.SMOGIndex()
+        print 'ColemanLiauIndex: ', rd.ColemanLiauIndex()
+        print 'LIX: ', rd.LIX()
+        print 'RIX: ', rd.RIX()
+        break
+    pp.pprint(author['d2a64c8cda3ca7eb0a29f38a9ee59a71'])
+
+main(author)
+
+
 
 def links():
 
@@ -66,18 +100,4 @@ def punc(author):
     df.to_csv(outdir+'punctuation_distribution.csv')
 
 
-def main(author):
 
-    for file in filter_xml(authorFileNames):
-        author_id = file.split('.')[0]
-        file_path = directory+"/"+file
-
-        # Get features
-        author[author_id]['punctuation'] = punctu.extract_from_xml(file_path)
-        author[author_id]['hyperlinks_info'] = html_links.get_hyperlink_info(file_path)
-        author[author_id]['pos'] = pos_tagging.distribution(pos_tagging.get_POS_Tags(file_path))
-        author[author_id]['NER'] = NER_tagger.get_NER_tags(file_path)
-        break
-    pp.pprint(author['d2a64c8cda3ca7eb0a29f38a9ee59a71'])
-
-main(author)
