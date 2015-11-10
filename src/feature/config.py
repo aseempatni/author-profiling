825f19dd968d7ce2a883fc8e7a9323e347e4db7c
Aseem Patni <apatnip@gmail.com>
2015-11-10 22:51:05 +0530

    Added template for configuration file.

--- src/feature/config.py
+++
@@ -1,9 +0,0 @@
-
-input_blog_directory = '../../data/koppel/blogs'
-output_dump_directory = 'output/'
-starting_index = 0
-count = 5
-
-outdir = output_dump_directory
-directory = input_blog_directory
-ending_index = starting_index + count - 1
