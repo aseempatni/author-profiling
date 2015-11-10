3f7882a85e75be99531734698eaa1c22b91cb160
Aseem Patni <apatnip@gmail.com>
2015-11-10 21:33:58 +0530

    Done readability & separeted extract feature function.

---
+++ src/feature/av_readability.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from readability import readability as r
+def av_readability(docs):
+    c = Counter()
+    for doc in docs:
+        rd = r.Readability(doc)
+        read = {}
+        read["ARI"] = rd.ARI()
+        read["FleschReadingEase"] = rd.FleschReadingEase()
+        read['FleschKincaidGradeLevel'] = rd.FleschKincaidGradeLevel()
+        read['GunningFogIndex'] = rd.GunningFogIndex()
+        read['SMOGIndex'] = rd.SMOGIndex()
+        read['ColemanLiauIndex'] = rd.ColemanLiauIndex()
+        read['LIX'] = rd.LIX()
+        read['RIX'] = rd.RIX()
+        c.update(read)
+    for key, value in c.iteritems():
+        c[key] = value/len(docs)
+    return dict(c)
+
+
